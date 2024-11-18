65e525cb4cfa0fa0d2334b4b28be896079f1f265
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-11-18 17:08:29 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
-#========================================================================================================================
+cd openwrt
+# wireless
+rm -rf files/etc/config/wireless
+rm -rf files/etc/modules.d/wireless_enable
+# geodata
+wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+# Add luci-theme-opentomcat
+git clone https://github.com/jixiewang201907/luci-theme-opentomcat.git package/luci-theme-opentomcat
+# Add luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# Add amlogic
+#git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+# Add luci-app-passwall
+#git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+#git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+# Add luci-app-bypass
+git clone https://github.com/jixiewang201907/by-pass.git package/by-pass
+# Add luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# Add luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# other
-# rm -rf package/emortal/{autosamba,ipv6-helper}
-
+./scripts/feeds update -a
+./scripts/feeds install -a
