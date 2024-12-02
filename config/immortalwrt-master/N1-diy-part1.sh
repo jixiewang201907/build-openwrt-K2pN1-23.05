4111dcbc715efad7d5135173ddc93ef5cf864bb3
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-02 16:54:18 +0800

    Update N1-diy-part1.sh

--- config/immortalwrt-master/N1-diy-part1.sh
+++ config/immortalwrt-master/N1-diy-part1.sh
@@ -10,7 +10,7 @@ rm -rf feeds/luci/applications/luci-app-amlogic
 rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-passwall2
 # Add packages
-git clone https://github.com/ophub/luci-app-amlogic.git package/amlogic
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git package/amlogic
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
 # Add luci-app-bypass
 git clone https://github.com/jixiewang201907/by-pass.git package/bypass
@@ -19,4 +19,4 @@ git clone https://github.com/pymumu/openwrt-smartdns.git packages/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git packages/luci-app-smartdns
 # Add luci-app-passwall
 #git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
