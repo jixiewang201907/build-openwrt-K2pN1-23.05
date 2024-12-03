834aa73266c73d51e4b2bee2c8b91e609962673f
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-03 15:42:20 +0800

    Update N1-diy-part1.sh

--- config/immortalwrt-master/N1-diy-part1.sh
+++ config/immortalwrt-master/N1-diy-part1.sh
@@ -19,3 +19,6 @@ git clone https://github.com/pymumu/luci-app-smartdns.git packages/luci-app-smar
 # Add luci-app-passwall
 #git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+
+./scripts/feeds update -a
+./scripts/feeds install -a
