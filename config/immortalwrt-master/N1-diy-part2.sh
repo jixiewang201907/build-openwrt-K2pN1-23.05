860635e50cd721d3f1f343d5e33f60206f278615
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2025-02-11 10:40:28 +0800

    Update N1-diy-part2.sh

--- config/immortalwrt-master/N1-diy-part2.sh
+++ config/immortalwrt-master/N1-diy-part2.sh
@@ -2,7 +2,7 @@
 cd openwrt
 # Add a feed source
 echo "src-git amlogic https://github.com/ophub/luci-app-amlogic.git;main" >> "feeds.conf.default"
-#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git SSRplus https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
