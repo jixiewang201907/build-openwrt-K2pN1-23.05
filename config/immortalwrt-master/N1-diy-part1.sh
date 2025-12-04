a3a984f4ddd9b720d628619597d5beb13f9db9d9
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2025-12-04 22:11:01 +0800

    Update N1-diy-part1.sh

--- config/immortalwrt-master/N1-diy-part1.sh
+++ config/immortalwrt-master/N1-diy-part1.sh
@@ -35,5 +35,9 @@ git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/application
 #git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 
+# Add luci-app-adguardhome
+git clone https://github.com/AdguardTeam/AdGuardHome.git feeds/packages/net/AdGuardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a 
